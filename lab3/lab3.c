14ae4bd5b5057fc1669e6e0a4bdec2d8159a9995
Michael Shihrer <michael.shihrer@gmail.com>
2015-02-27 03:45:01 -0700

    Not sure what happened, but this should be the final commit for lab3

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -8,11 +8,11 @@
 
 int get_input();
 int is_valid(int input);
-void print_pattern();
+void print_pattern(int input);
 
 int main(void)
 {
-	get_input();
+	print_pattern(get_input());
 }
 
 int get_input()
@@ -21,31 +21,58 @@ int get_input()
 	do
 	{
 		printf("Enter an odd number between 1 and 9 > ");
-		scanf("$d", &input);
-	}while(is_valid(input));
+		scanf("%d", &input);
+	}while(!is_valid(input));
+
+	return input;
 }
 
 int is_valid(int input)
 {
 	if((input % 2) == 0) 
 	{
-		printf("You have entered an even number.  Please try again./n");
+		printf("You have entered an even number.  Please try again.\n");
 		return 0;
 	}
 	else if(input > 9)
 	{
-		printf("You have entered a number greater than 9.  Please try again./n");
+		printf("You have entered a number greater than 9.  Please try again.\n");
 		return 0;
 	}
 	else if(input < 1)
 	{
-		printf("You have entered a number less than 1.  Please try again./n");
+		printf("You have entered a number less than 1.  Please try again.\n");
 		return 0;
 	}
 
 	return 1;
 }
 
-void print_pattern()
+void print_pattern(int input)
 {
+	for(int row = 1; row <= input; row+=2)
+	{
+		printf("%*s", input - row,"");
+
+		for(int col = 1; col <= row; col++)
+		{
+			printf("%d",col);
+			printf("%*s", 1,"");
+		}
+		
+		printf("\n");
+	}
+	
+	for(int row = input - 2; row >= 1; row-=2)
+	{
+		printf("%*s",input-row,"");
+		
+		for(int col = 1; col <= row; col++)
+		{
+			printf("%d",col);
+			printf("%*s",1,"");
+		}
+
+		printf("\n");
+	}
 }
