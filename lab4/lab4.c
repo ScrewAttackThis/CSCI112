ad84bd4c2b34c2473718e60011be530e976b52cf
Michael <michael.shihrer@gmail.com>
2015-03-06 02:13:42 -0700

    added computeValues function and error checking in computeTan

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -1,6 +1,16 @@
+/*
+ * Michael Shihrer
+ * Lab 4, CSCI 112
+ * 5 Mar 15
+ */
+
 #include <stdio.h>
 #include <math.h>
 #include <stdbool.h>
+
+//Constants
+#define PI 3.14159
+#define LOOP_LIMIT 90
 
 //MENU enum definition
 typedef enum MENU {
@@ -10,18 +20,14 @@ typedef enum MENU {
 	QUIT
 } menu_t;
 
-/*
- * Michael Shihrer
- * Lab 4, CSCI 112
- * 5 Mar 15
- */
 
 //Function prototypes
 void userMenu();
-bool is_valid(menu_t input);
 double computeSin(double angle);
 double computeCos(double angle);
 double computeTan(double angle);
+void computeValues();
+double degreeToRadian(double angle);
 
 int main(void)
 {
@@ -41,36 +47,57 @@ void userMenu()
 		switch(input)
 		{
 			case SINE:
+				computeValues(computeSin);
 				break;
 			case	COSINE:
+				computeValues(computeCos);
 				break;
 			case TANGENT:
+				computeValues(computeTan);
 				break;
 			case QUIT:
 				break;
 			default:
-				printf("%d is an invalid option.  Please try again.", input);
+				printf("%d is an invalid option.  Please try again.\n", input);
+				break;
 		}
 	}while(!(input == QUIT));
 }
 
-bool is_valid(menu_t input)
-{
-	printf("%d", input);
-	return true;
-}
-
 double computeSin(double angle)
 {
+	printf("Compute sine");
 	return 0.0;
 }
 
 double computeCos(double angle)
 {
+	printf("Compute cosine");
 	return 0.0;
 }
 
 double computeTan(double angle)
 {
+	if(angle == 90 || angle == 270)
+	{
+		printf("UNDEFINED");
+	}
+	else
+	{
+		printf("Compute tangent");
+	}
 	return 0.0;
+}
+
+void computeValues(double (*computeFunction)(double))
+{
+	for(int i = 0; i <= LOOP_LIMIT; i+=15)
+	{
+		(*computeFunction)(i);
+	}
+}
+
+double degreeToRadian(double angle)
+{
+	return PI * angle/180;
 }
