78eb87313baa0563e86232eeee8a20bb5361c998
Michael <michael.shihrer@gmail.com>
2015-03-27 02:23:48 -0600

    new barcode

--- lab6/lab6.c
+++ lab6/lab6.c
@@ -17,7 +17,11 @@ int main(void)
   int validInput;
 
   //Barcode aray
-  int barcode[12] = {0,7,9,4,0,0,8,0,4,5,0,1};
+  int barcode[12] = {0,1,1,1,1,0,8,5,6,8,0,7};
+  //{0,7,9,4,0,0,8,0,4,5,0,1}
+  //0 1 1 1 1 0 8 5 6 8 0 7
+  //0 5 1 0 0 0 1 3 8 1 0 1
+  //0 2 4 0 0 0 1 6 2 8 6 0
 
   // Print welcome message
   printf("\nEnter a bar code to check.  Seperate digits with a space > \n");
