cc3f87e84e3e716d384d96190cb9cf1018d2d84c
Michael <michael.shihrer@gmail.com>
2015-04-24 01:41:26 -0600

    asdf

--- lab8/lab8.c
+++ lab8/lab8.c
@@ -68,11 +68,11 @@
       scanf("%d", newStudent.grades + i);
    }
 
-   /*printf("Grades entered: ");
+   printf("Grades entered: ");
    for(int i = 0; i < numberOfGrades; i++)
    {
      printf("%d", *(newStudent.grades + i));
-   }*/
+   }
 
    return newStudent;
  }
