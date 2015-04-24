268a6809cc7d8b6b0557da206b00ad7b3388eefb
Michael <michael.shihrer@gmail.com>
2015-04-24 02:24:28 -0600

    asdf

--- lab8/lab8.c
+++ lab8/lab8.c
@@ -51,7 +51,7 @@
  student inputStudent(int numberOfGrades)
  {
    student newStudent;
-   newStudent.grades = (float *) malloc(numberOfGrades * sizeof(float));
+   newStudent->grades = (float *) malloc(numberOfGrades * sizeof(float));
 
 
    printf("Enter information for student:\n");
@@ -65,9 +65,7 @@
 
    for(int i = 0; i < numberOfGrades; i++)
    {
-     int newGrade;
-      scanf("%d", &newGrade);
-      *(newStudent.grades + i) = newGrade;
+     scanf("%d", newStudent->grades + i);
    }
 
    printf("Grades entered: ");
